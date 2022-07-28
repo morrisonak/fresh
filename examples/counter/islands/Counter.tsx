51e0118f83b4ef809bab677b1acb8e91460b6b60
morrisonak <57382686+morrisonak@users.noreply.github.com>
2022-07-27 20:57:27 -0500

    Update Counter.tsx

--- examples/counter/islands/Counter.tsx
+++ examples/counter/islands/Counter.tsx
@@ -11,7 +11,7 @@ export default function Counter(props: CounterProps) {
   const [count, setCount] = useState(props.start);
   return (
     <div>
-      <p>{count}</p>
+      <p>Micah's {count}</p>
       <button onClick={() => setCount(count - 1)} disabled={!IS_BROWSER}>
         -1
       </button>
